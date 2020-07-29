b65b4b101b05f9da57be8d0fb702e3f403827f74
Robertjlrea <robertjlrea@gmail.com>
2020-07-29 08:40:57 +0100

    First build minor movement and hit detection added

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    public float rotatespeed;
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.RotateAround(target.position, Vector3.up, rotatespeed * Time.deltaTime);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+
+        void Update()
+        {
+            UpdateAnimator();
+
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+    }
+}
