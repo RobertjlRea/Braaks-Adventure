da8caeac312f88d74cb41fa4cbbda709cf4bc928
Robertjlrea <robertjlrea@gmail.com>
2020-09-08 23:15:28 +0100

    Dictonary able to save multiple scenes

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -10,45 +10,58 @@ public class SavingSystem : MonoBehaviour
 {
  public void Save(string saveFile)
  {
-     string path = GetPathFromSaveFile(saveFile);
-     print ("Saving to " + path);
-     using (FileStream stream = File.Open(path, FileMode.Create))
-     {
-        
-        BinaryFormatter formatter = new BinaryFormatter();
-        formatter.Serialize(stream, CaptureState());
-
-     }
+     SaveFile(saveFile, CaptureState());
  }
 
   public void Load(string saveFile)
  {
-     string path = GetPathFromSaveFile(saveFile);
-     print ("Loading from " + GetPathFromSaveFile(saveFile));
+     RestoreState(LoadFile(saveFile));
+  }
+  
+private Dictionary<string, object> LoadFile(string saveFile)
+    {
+    string path = GetPathFromSaveFile(saveFile);
+
+     print("Loading from " + path);
      using (FileStream stream = File.Open(path, FileMode.Open))
      {
         BinaryFormatter formatter = new BinaryFormatter();
-        RestoreState(formatter.Deserialize(stream));
+        return (Dictionary<string, object>) formatter.Deserialize(stream);
      }
-  }
+    }
 
-   private object CaptureState()
-  {
-      Dictionary<string, object> state = new Dictionary<string, object>();
-      foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
-      {
-          state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
-         
-      }
+  private void SaveFile(string saveFile, object state)
+    {
+     string path = GetPathFromSaveFile(saveFile);
+     print ("Saving to " + path);
+     using (FileStream stream = File.Open(path, FileMode.Create))
+    
+     {
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, state);
+        
+     }
+    }
+
+
+     private Dictionary<string, object> CaptureState()
+        {
+              Dictionary<string, object> state = new Dictionary<string,object>();
+              foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+              {
+                  state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
+              }
       return state;
-  }
+        }
   
-  private void RestoreState(object state)
+
+  
+  private void RestoreState(Dictionary<string, object> state)
   {
-      Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+      
       foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
       {
-          saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+          saveable.RestoreState(state[saveable.GetUniqueIdentifier()]);
       }
   }
   private byte[] SerializeVector(Vector3 vector)
