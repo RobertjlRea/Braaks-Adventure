ce1c1063749178424b9c2738b251a540d697174a
Robertjlrea <robertjlrea@gmail.com>
2020-10-12 09:04:13 +0100

    unarmed issue

---
+++ Assets/Scripts/Combat/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+public class Pickup : MonoBehaviour
+{
+    [SerializeField] Weapon weapon = null;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<Fighter>().EquipWeapon(weapon);
+            Destroy(gameObject);
+        }
+    }
+}
+}
